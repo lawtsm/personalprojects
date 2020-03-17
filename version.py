d4ccdfa900e4a5ee9b45e56bf47db7e7578ce526
Lawrence Toh <lolenz@gmail.com>
2020-03-17 16:05:05 -0400

    update

---
+++ version.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 18:38:26 2020
+
+@author: lolen
+"""
+
+import plotly
+print("plotly version: {}".format(plotly.__version__))
+import plotly.express as px
+import plotly.graph_objects as go
+
+import matplotlib
+print(matplotlib.__version__)
